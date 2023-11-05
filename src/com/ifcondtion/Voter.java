f7cf867f7d2ca839067a923a47c6383b7dc3658c
ADMIN <ADMIN@DESKTOP-V3LBF7M>
2023-11-05 21:30:01 +0530

    Push 2nd Project

---
+++ src/com/ifcondtion/Voter.java
@@ -0,0 +1,20 @@
+package com.ifcondtion;
+
+public class Voter {
+
+	public static void main(String[] args) { //if and else conditions
+
+		int age = 20;
+
+		if (age >= 18) {
+
+			System.out.println("Age Eligibility to Vote is: " + age);
+
+		}
+		else {
+			System.out.println("Your not Eligible to vote: "+age);
+		}
+
+	}
+
+}
